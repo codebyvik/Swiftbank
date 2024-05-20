ae5563a737cbd6412be6cc0176479cb8e2cebaab
Vikas B R <vikasvickycoorg@gmail.com>
2024-05-21 01:13:35 +0530

    initial push , added material ui , styled components and logos

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to swiftbank
+      <br></br>
+      <Button variant="contained">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
